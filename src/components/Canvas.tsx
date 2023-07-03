367c1138dd8179cef5a5a3fd449d462c6960c615
Michael Berry <mcberry@gmail.com>
2023-07-03 10:15:05 -0400

    half baked undo/redo

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 interface CanvasProps {
   canvasWidth: number;
@@ -6,14 +6,52 @@ interface CanvasProps {
 }
 
 const Canvas: React.FC<CanvasProps> = ({ canvasWidth, canvasHeight }) => {
-  const [selectedColor, setSelectedColor] = useState('');
+  const [selectedColor, setSelectedColor] = useState('#000000');
   const [selectedThickness, setSelectedThickness] = useState(15);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [undoStack, setUndoStack] = useState<string[]>([]);
+  const [redoStack, setRedoStack] = useState<string[]>([]);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const handleCanvasClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    const canvas = event.currentTarget.querySelector(
-      'canvas'
-    ) as HTMLCanvasElement | null;
-    if (canvas) {
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+      if (context) {
+        context.lineCap = 'round';
+      }
+    }
+  }, []);
+
+  const startDrawing = () => {
+    setIsDrawing(true);
+  };
+
+  const stopDrawing = () => {
+    setIsDrawing(false);
+  };
+
+  const handleCanvasMouseClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = event.currentTarget;
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    if (selectedColor) {
+      const canvasContext = canvas.getContext('2d');
+
+      if (canvasContext) {
+        canvasContext.fillStyle = selectedColor;
+        canvasContext.beginPath();
+        canvasContext.arc(x, y, selectedThickness, 0, 2 * Math.PI);
+        canvasContext.fill();
+      }
+    }
+  };
+
+  const handleCanvasMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    if (isDrawing && canvasRef.current) {
+      const canvas = canvasRef.current;
       const rect = canvas.getBoundingClientRect();
       const x = event.clientX - rect.left;
       const y = event.clientY - rect.top;
@@ -21,13 +59,36 @@ const Canvas: React.FC<CanvasProps> = ({ canvasWidth, canvasHeight }) => {
       if (selectedColor) {
         const canvasContext = canvas.getContext('2d');
         if (canvasContext) {
-          canvasContext.fillStyle = selectedColor;
-          canvasContext.beginPath();
-          canvasContext.arc(x, y, selectedThickness, 0, 2 * Math.PI);
-          canvasContext.fill();
+          canvasContext.strokeStyle = selectedColor;
+          canvasContext.lineWidth = selectedThickness;
+          canvasContext.lineTo(x, y);
+          canvasContext.stroke();
         }
       }
     }
+  };
+
+  const handleCanvasMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const canvasContext = canvas.getContext('2d');
+      if (canvasContext) {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        canvasContext.beginPath();
+        canvasContext.moveTo(x, y);
+        canvasContext.stroke();
+        
+      }
+      startDrawing();
+      setRedoStack([]);
+    }
+  };
+
+  const handleCanvasMouseUp = () => {
+    stopDrawing();
+    saveCanvasState();
   };
 
   const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,6 +99,62 @@ const Canvas: React.FC<CanvasProps> = ({ canvasWidth, canvasHeight }) => {
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
     setSelectedThickness(Number(event.target.value));
+  };
+
+  const handleClearCanvas = () => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const canvasContext = canvas.getContext('2d');
+      if (canvasContext) {
+        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+      }
+      setUndoStack([]);
+      setRedoStack([]);
+    }
+  };
+
+  const handleUndo = () => {
+    if (canvasRef.current && undoStack.length > 0) {
+      const lastAction = undoStack[undoStack.length - 1];
+      const canvas = canvasRef.current;
+      const canvasContext = canvas?.getContext('2d');
+      if (canvasContext) {
+        setUndoStack(undoStack.slice(0, undoStack.length - 1));
+        setRedoStack([...redoStack, canvas.toDataURL()]);
+        const img = new Image();
+        img.src = lastAction;
+        img.onload = () => {
+          canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+          canvasContext.drawImage(img, 0, 0);
+        };
+      }
+    }
+  };
+
+  const handleRedo = () => {
+    if (canvasRef.current && redoStack.length > 0) {
+      const lastRedoAction = redoStack[redoStack.length - 1];
+      const canvas = canvasRef.current;
+      const canvasContext = canvas?.getContext('2d');
+      if (canvasContext) {
+        setUndoStack([...undoStack, lastRedoAction]);
+        setRedoStack(redoStack.slice(0, redoStack.length - 1));
+        const img = new Image();
+        img.src = lastRedoAction;
+        img.onload = () => {
+          canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+          canvasContext.drawImage(img, 0, 0);
+        };
+      }
+    }
+  };
+
+  const saveCanvasState = () => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const canvasData = canvas.toDataURL();
+      setUndoStack([...undoStack, canvasData]);
+    }
   };
 
   return (
@@ -63,6 +180,28 @@ const Canvas: React.FC<CanvasProps> = ({ canvasWidth, canvasHeight }) => {
           <option value={20}>20px</option>
           <option value={25}>25px</option>
         </select>
+
+        <button
+          onClick={handleClearCanvas}
+          style={{ marginTop: '10px', width: '100%' }}
+        >
+          Clear
+        </button>
+
+        <button
+          onClick={handleUndo}
+          style={{ marginTop: '10px', width: '100%' }}
+        >
+          Undo
+        </button>
+
+        <button
+          onClick={handleRedo}
+          style={{ marginTop: '10px', width: '100%' }}
+        >
+          Redo
+        </button>
+
       </div>
       <div
         style={{
@@ -72,13 +211,18 @@ const Canvas: React.FC<CanvasProps> = ({ canvasWidth, canvasHeight }) => {
           marginLeft: '10px',
           position: 'relative',
         }}
-        onClick={handleCanvasClick}
       >
         {/* Canvas contents */}
         <canvas
+          ref={canvasRef}
           width={canvasWidth}
           height={canvasHeight}
           style={{ cursor: 'crosshair' }}
+          onMouseDown={handleCanvasMouseDown}
+          onMouseMove={handleCanvasMouseMove}
+          onMouseUp={handleCanvasMouseUp}
+          onMouseLeave={handleCanvasMouseUp}
+          onClick={handleCanvasMouseClick}
         />
       </div>
     </div>
